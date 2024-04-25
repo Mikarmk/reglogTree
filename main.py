1fc0604ac4194fd5d7572e13945a049719ed5d92
Murat <144884820+Mikarmk@users.noreply.github.com>
2024-04-25 10:12:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,47 +5,54 @@ from oauth2client.service_account import ServiceAccountCredentials
 # Подключение к Google Sheets API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('your_credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('your-credentials.json', scope)
 client = gspread.authorize(creds)
-sheet = client.open('your_google_sheet_name').sheet1
+sheet = client.open('your-google-sheet').sheet1
 
-# Страница регистрации
-def registration():
-    st.title('Регистрация')
-    username = st.text_input('Введите имя пользователя')
-    password = st.text_input('Введите пароль', type='password')
+# Регистрация пользователя
+def register_user(username, password):
+    existing_users = sheet.col_values(1)
+    if username in existing_users:
+        return "Пользователь с таким именем уже существует"
+    else:
+        sheet.append_row([username, password])
+        return "Регистрация прошла успешно"
 
+# Авторизация пользователя
+def login_user(username, password):
+    user_data = sheet.find(username)
+    if user_data:
+        stored_password = sheet.cell(user_data.row, 2).value
+        if password == stored_password:
+            return "Авторизация успешна"
+        else:
+            return "Неверный пароль"
+    else:
+        return "Пользователь не найден"
+
+# Сохранение сообщений пользователя
+def save_message(username, message):
+    messages_sheet = client.open('your-google-sheet').get_worksheet(1)
+    messages = messages_sheet.col_values(1)
+    if len(messages) >= 10:
+        messages_sheet.delete_row(1)
+    messages_sheet.append_row([message])
+
+# Интерфейс Streamlit
+st.title('Регистрация и авторизация')
+
+action = st.selectbox('Выберите действие', ['Регистрация', 'Авторизация'])
+
+if action == 'Регистрация':
+    username = st.text_input('Имя пользователя')
+    password = st.text_input('Пароль', type='password')
     if st.button('Зарегистрироваться'):
-        if username in sheet.col_values(1):
-            st.error('Пользователь с таким именем уже зарегистрирован')
-        else:
-            sheet.append_row([username, password])
-            st.success('Регистрация прошла успешно')
+        result = register_user(username, password)
+        st.write(result)
 
-# Страница авторизации
-def login():
-    st.title('Авторизация')
-    username = st.text_input('Введите имя пользователя')
-    password = st.text_input('Введите пароль', type='password')
-
+if action == 'Авторизация':
+    username = st.text_input('Имя пользователя')
+    password = st.text_input('Пароль', type='password')
     if st.button('Войти'):
-        user_data = sheet.get_all_records()
-        for user in user_data:
-            if user['Username'] == username and user['Password'] == password:
-                st.success('Успешный вход')
-                break
-        else:
-            st.error('Неверное имя пользователя или пароль')
-
-# Основная часть приложения
-def main():
-    st.sidebar.title('Навигация')
-    page = st.sidebar.radio('Выберите страницу', ['Регистрация', 'Авторизация'])
-
-    if page == 'Регистрация':
-        registration()
-    elif page == 'Авторизация':
-        login()
-
-if __name__ == '__main__':
-    main()
+        result = login_user(username, password)
+        st.write(result)
